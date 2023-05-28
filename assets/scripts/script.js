bbea41fa55cbd3022c58355a8bfdf26e255f760b
Murad <agamedov94@mail.ru>
2023-05-28 12:03:48 +0400

    try

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,3 +16,39 @@ $('.header__carusel').slick({
       }
    ]
  });
+
+ $('.try__carusel').slick({
+   infinite: true,
+   dots: false,
+   infinite: true,
+   speed: 300,
+   slidesToShow: 4,
+   prevArrow: $('.try__carusel__left'),
+   nextArrow: $('.try__carusel__right'),
+   responsive: [
+      {
+         breakpoint: 1240,
+         settings: {
+            arrows: false
+          }
+      },
+      {
+         breakpoint: 991,
+         settings: {
+            slidesToShow: 3,
+          }
+      },
+      {
+         breakpoint: 768,
+         settings: {
+            slidesToShow: 2,
+          }
+      },
+      {
+         breakpoint: 500,
+         settings: {
+            slidesToShow: 1,
+          }
+      }
+   ]
+ });
