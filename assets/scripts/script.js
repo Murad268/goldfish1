8d93a2eab3c8db90f749cf25b56df0ca295eed3a
Murad <agamedov94@mail.ru>
2023-05-28 12:31:27 +0400

    carusels

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -17,7 +17,7 @@ $('.header__carusel').slick({
    ]
  });
 
- $('.try__carusel').slick({
+ $('.try__carusel.try').slick({
    infinite: true,
    dots: false,
    infinite: true,
@@ -25,6 +25,42 @@ $('.header__carusel').slick({
    slidesToShow: 4,
    prevArrow: $('.try__carusel__left'),
    nextArrow: $('.try__carusel__right'),
+   responsive: [
+      {
+         breakpoint: 1240,
+         settings: {
+            arrows: false
+          }
+      },
+      {
+         breakpoint: 991,
+         settings: {
+            slidesToShow: 3,
+          }
+      },
+      {
+         breakpoint: 768,
+         settings: {
+            slidesToShow: 2,
+          }
+      },
+      {
+         breakpoint: 500,
+         settings: {
+            slidesToShow: 1,
+          }
+      }
+   ]
+ });
+
+ $('.special__try .try__carusel').slick({
+   infinite: true,
+   dots: false,
+   infinite: true,
+   speed: 300,
+   slidesToShow: 4,
+   prevArrow: $('.special__try__left'),
+   nextArrow: $('.special__try__right'),
    responsive: [
       {
          breakpoint: 1240,
